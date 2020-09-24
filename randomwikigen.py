60fec11919700017c4ab33d1b8b83bfd81dfa673
Ubuntu <ubuntu@ip-172-31-45-140.eu-west-3.compute.internal>
2020-09-24 20:16:19 +0000

    Small improvements

--- randomwikigen.py
+++ randomwikigen.py
@@ -1,12 +1,4 @@
 def randomwikigen():
 	import requests
-	i = 0
-	how_many_links = 2
-	while i < how_many_links:
-
-		response = requests.get('https://it.wikipedia.org/wiki/Speciale:PaginaCasuale')
-		return("\n"+response.url)
-		i = i+1
-
-	if i>= how_many_links:
-		exit(2)
+	response = requests.get('https://it.wikipedia.org/wiki/Speciale:PaginaCasuale')
+	return("\n"+response.url)
