926193c9467e409e49774443080c8c8c8bb2d396
Silvio Santoriello <silviosanto6605@gmail.com>
2022-12-29 00:57:36 +0100

    update with slash commands

--- discord-bot.py
+++ discord-bot.py
@@ -1,18 +1,27 @@
 import os
 import randomwikigen
 import discord
-from dotenv import load_dotenv
+from discord import app_commands
 from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='--')
 
+bot = commands.Bot(command_prefix="--",intents=discord.Intents.all())
 
-client = discord.Client()
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands)")
+        
+    except Exception as e:
+        print(e)
+        
+@bot.tree.command(name="start",description="Starts the bot, which returns two random links from wikipedia.it")
+async def start(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Inizio: {randomwikigen.randomwikigen()},\nFine:{randomwikigen.randomwikigen()}")
 
-@bot.command(name='wikirun', help='Generate 2 random Wikipedia links')
-async def random_links (ctx):
-    await ctx.channel.send("Inizio: " +randomwikigen.randomwikigen())
-    await ctx.channel.send("Fine: " +randomwikigen.randomwikigen())
 bot.run(token)
